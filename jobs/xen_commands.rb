7366e0179509ca3835acd412ecc9d7934fe2987c
Mike Bailey <mike@bailey.net.au>
2008-09-16 17:21:24 +1000

    Command line access to Xen::Command lib

---
+++ jobs/xen_commands.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#
+# xen_commands.rb
+#
+# Command line access to ruby-xen's Xen::Command
+#
+require 'rubygems'
+require 'ruby-xen'
+
+# show usage info if wrong args
+# - list commands 
+command = ARGV.shift 
+Xen::Command.send(command, ARGV)
