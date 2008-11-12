5f44e4cb5a0a4542255a02dbb9394cfe18dfe157
Mike Bailey <mike@bailey.net.au>
2008-11-12 11:25:52 +1100

    Monkey patch ruby-xen's Xen::Slice to use backgroundjob

---
+++ lib/xen/slice.rb
@@ -0,0 +1,56 @@
+class Xen::Slice # We get this from the ruby-xen gem
+
+  XEN_CMD_RUNNER = '/usr/local/bin/ruby jobs/xen_cmd.rb' # XXX don't hard code path
+  
+  # XXX Perhaps DRY this up by extracting commonality
+  def shutdown
+    Bj.submit "#{XEN_CMD_RUNNER} shutdown_instance #{name} true", :tag => "#{name}.shutdown_instance"
+  end
+  
+  def create
+    Bj.submit "#{XEN_CMD_RUNNER} create_image --hostname=#{name}", :tag => "#{name}.create_image"
+  end
+  
+  def create_backup(version=nil)
+    version ||= Time.now.strftime('%Y%m%d')
+    Bj.submit "#{XEN_CMD_RUNNER} backup_slice #{name} #{version} :blocking", :tag => "#{name}.backup_slice"
+  end
+  
+  def shutting_down?
+    # XXX What happens if bj fails and we get a whole lot of pending requests?
+    # XXX Perhaps run a sweeper under cron to clean them up.
+    # XXX Perhaps get most recent shutdown command and return true if it is running or pending
+    Bj.table.job.find(:first, :conditions => "tag = '#{name}.shutdown_instance' and state in ('running', 'pending')")
+  end
+  
+  def pending_creation?
+    # XXX Same issues as :shutting_down?
+    Bj.table.job.find(:first, :conditions => "tag = '#{name}.create' and state in ('running', 'pending')")
+  end
+  
+  # not working yet
+  def backup_pending?
+    Bj.table.job.find(:first, :conditions => "tag = '#{name}.backup_slice' and state in ('pending')")
+  end
+  
+  alias _state state
+  def state
+    if _state == :running
+      return shutting_down? ? :shutting_down : :running
+    else
+      return pending_creation? ? :pending_creation : :stopped
+    end
+  end
+  
+  alias _backups backups
+  def backups
+    list = _backups
+    list << Xen::Backup.new(name, :version => "pending") if backup_pending?
+    list
+  end
+  
+  def running?
+    state == :running
+  end
+  
+end
