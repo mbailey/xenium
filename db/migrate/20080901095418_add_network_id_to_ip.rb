d1d4ae0d93ebeb71d6d74373613128c9afe49e6e
Mike Bailey <mike@bailey.net.au>
2009-01-16 16:44:54 +1100

    Migration tidy up

--- db/migrate/20080901095418_add_network_id_to_ip.rb
+++
@@ -1,9 +0,0 @@
-class AddNetworkIdToIp < ActiveRecord::Migration
-  def self.up
-    add_column :ips, :network_id, :integer
-  end
-
-  def self.down
-    remove_column :ips, :network_id
-  end
-end
