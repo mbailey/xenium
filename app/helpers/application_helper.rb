79681c77c994225830fd8b7e1218ec04e2d2c552
Mike Bailey <mike@bailey.net.au>
2009-07-04 16:26:54 +1000

    updated start/stop panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,12 @@ module ApplicationHelper
   def slice_control_buttons(slice)
     result = []
     if slice.state == :running 
-      result << 'start'
+      result = '<span class="active">running</span>'
       result << link_to_remote('shutdown', :url => shutdown_slice_path(slice.name), :method => :put, :confirm => 'Are you sure?', :update => "slice_controls_#{slice.name}")
     elsif slice.state == :stopped
+      result = '<span class="inactive">stopped</span>'
       result << link_to_remote('start', :url => start_slice_path(slice.name),  :method => :put, :update => "slice_controls_#{slice.name}")
-      result << 'shutdown'
     end
-    "#{slice.state.to_s} [#{result.join ' | '}]"
   end
   
   def slice_toggle_autostart(slice)
